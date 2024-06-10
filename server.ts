6a7b2ea97a780dc769dc16b796e390107e783cc2
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-10 22:13:23 +0600

    --UPDATED

--- server.ts
+++ server.ts
@@ -1,13 +1,16 @@
-const cors = require("cors");
-const express = require("express");
-const dotenv = require("dotenv").config({ path: "./.env" });
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
 import cartRoutes from "@infrastructure/routes/cartRoutes";
 import componentRoutes from "@infrastructure/routes/componentRoutes";
 import blogRoutes from "@infrastructure/routes/blogRoutes";
 
 const app = express();
-const port = process.env.PORT;
-const bodyParser = require("body-parser");
+const port = process.env.PORT || 4001;
 
 const corsOptions = {
   // origin: "https://spark-admin-production.up.railway.app",
@@ -15,21 +18,29 @@ const corsOptions = {
   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
   allowedHeaders: ["Authorization", "Content-Type"],
 };
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-//Component routes
+// Middleware to log incoming requests
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// Component routes
 app.use("/api/components", componentRoutes);
 
-//Cart routes
+// Cart routes
 app.use("/api/carts", cartRoutes);
 
-// Blog Routes
+// Blog routes
 app.use("/api/blog", blogRoutes);
 
+// Default route to handle unknown endpoints
 app.use((req, res, next) => {
   res.status(404).json({ message: "Endpoint not found" });
 });
@@ -43,3 +54,5 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+export default app;
