5deec877dd7de5c76451d2cc4c814b0b3c01eae4
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-10 13:18:36 +0500

    --SAVED

--- infrastructure/routes/componentRoutes.ts
+++ infrastructure/routes/componentRoutes.ts
@@ -5,10 +5,16 @@ const router = express.Router();
 
 router.post("/add", (req, res) => componentController.addComponent(req, res));
 
-router.get("/get-components", (req, res) => componentController.getComponents(req, res));
+router.get("/get-components", (req, res) =>
+  componentController.getComponents(req, res)
+);
 
-router.delete("/delete/:id", (req, res) => componentController.deleteComponent(req, res));
+router.delete("/delete/:id", (req, res) =>
+  componentController.deleteComponent(req, res)
+);
 
-router.post("/update/:id", (req, res) => componentController.updateComponent(req, res));
+router.post("/update/:id", (req, res) =>
+  componentController.updateComponent(req, res)
+);
 
 export default router;
