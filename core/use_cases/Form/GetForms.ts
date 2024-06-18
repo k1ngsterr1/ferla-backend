5ca027471b4e9e5fff51c29e9e1613f6681d8b1e
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-18 11:21:46 +0500

    --UPDATED

--- core/use_cases/Form/GetForms.ts
+++ core/use_cases/Form/GetForms.ts
@@ -5,18 +5,20 @@ import { FormRepository } from "@infrastructure/repositories/formRepository";
 const Code: string = process.env.WEBSITE_CODE;
 
 export default class GetForms {
-    private FormRepository: IFormRepository;
-    constructor() {
-        this.FormRepository = new FormRepository();
-    }
-    async execute(code: string, errors: ErrorDetails[]): Promise<Form[]> {
-        if(code !== Code){
-            errors.push(new ErrorDetails(403, "The website code is incorrect."));
-            return;
-        }
+  private FormRepository: IFormRepository;
+  constructor() {
+    this.FormRepository = new FormRepository();
+  }
+  async execute(code: string, errors: ErrorDetails[]): Promise<Form[]> {
+    console.log(code, Code);
 
-        const Forms = await this.FormRepository.findForms(errors);
-
-        return Forms;
+    if (code !== Code) {
+      errors.push(new ErrorDetails(403, "The website code is incorrect."));
+      return;
     }
+
+    const Forms = await this.FormRepository.findForms(errors);
+
+    return Forms;
+  }
 }
--- presentation/controllers/formController.ts
+++ presentation/controllers/formController.ts
@@ -5,76 +5,79 @@ import { AddFormRequest, DeleteFormRequest } from "@core/utils/Form/Request";
 import { ErrorDetails } from "@core/utils/utils";
 import { Request, Response } from "express";
 
-class FormController{
-    private addFormUseCase: AddForm;
-    private getFormsUseCase: GetForms;
-    private deleteFormUseCase: DeleteForm;
-    constructor(){
-        this.addFormUseCase = new AddForm();
+class FormController {
+  private addFormUseCase: AddForm;
+  private getFormsUseCase: GetForms;
+  private deleteFormUseCase: DeleteForm;
+  constructor() {
+    this.addFormUseCase = new AddForm();
+    this.getFormsUseCase = new GetForms();
+    this.deleteFormUseCase = new DeleteForm();
+  }
+  async addForm(req: Request, res: Response): Promise<any> {
+    const errors: ErrorDetails[] = [];
+    try {
+      const request: AddFormRequest = {
+        code: req.body.code,
+        name: req.body.name,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+        date: req.body.date,
+      };
+
+      await this.addFormUseCase.execute(request, errors);
+
+      if (errors.length > 0) {
+        return res.status(errors[0].code).json({ message: errors[0].details });
+      }
+
+      res.status(201).json({ message: "Added form succesfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error adding the form." });
     }
-    async addForm(req: Request, res: Response): Promise<any>{
-        const errors: ErrorDetails[] = [];
-        try{
-            const request: AddFormRequest = {
-                code: req.body.code,
-                name: req.body.name,
-                phoneNumber: req.body.phoneNumber,
-                email: req.body.email,
-                date: req.body.date,
-            }
+  }
 
-            await this.addFormUseCase.execute(request, errors);
+  async getForms(req: Request, res: Response): Promise<any> {
+    const errors: ErrorDetails[] = [];
+    try {
+      const code: string = req.params.code;
 
-            if(errors.length > 0){
-                return res.status(errors[0].code).json({ message: errors[0].details });
-            }
+      console.log("CODE:", code);
 
-            res.status(201).json({ message: "Added form succesfully" });
-        } catch(error){
-            console.log(error);
-            res.status(500).json({ message: "Error adding the form." });
-        }
+      const forms = await this.getFormsUseCase.execute(code, errors);
+
+      if (errors.length > 0) {
+        return res.status(errors[0].code).json({ message: errors[0].details });
+      }
+
+      res.status(200).json({ forms: forms });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error geting the Form." });
     }
+  }
 
-    async getForms(req: Request, res: Response): Promise<any>{
-        const errors: ErrorDetails[] = [];
-        try{
-            const code: string = req.params.code;
+  async deleteForm(req: Request, res: Response): Promise<any> {
+    const errors: ErrorDetails[] = [];
+    try {
+      const request: DeleteFormRequest = {
+        id: Number(req.params.id),
+        code: req.params.code,
+      };
 
-            const forms = await this.getFormsUseCase.execute(code, errors);
+      await this.deleteFormUseCase.execute(request, errors);
 
-            if(errors.length > 0){
-                return res.status(errors[0].code).json({ message: errors[0].details });
-            }
+      if (errors.length > 0) {
+        return res.status(errors[0].code).json({ message: errors[0].details });
+      }
 
-            res.status(200).json({ forms: forms });
-        } catch(error){
-            console.log(error);
-            res.status(500).json({ message: "Error geting the Form." });
-        }
+      res.status(200).json({ message: "Deleted form succesfully" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error deleting the form." });
     }
-
-    async deleteForm(req: Request, res: Response): Promise<any>{
-        const errors: ErrorDetails[] = [];
-        try{
-            const request: DeleteFormRequest = {
-                id: Number(req.params.id),
-                code: req.params.code
-            }
-
-            await this.deleteFormUseCase.execute(request, errors);
-
-            if(errors.length > 0){
-                return res.status(errors[0].code).json({ message: errors[0].details });
-            }
-
-            res.status(200).json({ message: "Deleted form succesfully" });
-        } catch(error){
-            console.log(error);
-            res.status(500).json({ message: "Error deleting the form." });
-        }
-    }
-
+  }
 }
 
 export default new FormController();
