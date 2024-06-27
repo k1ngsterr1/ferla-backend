7bbf3e1cd70e0f303d9227171cad4cc16648a506
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-28 01:14:19 +0600

    --DB MIGRATION

--- infrastructure/models/articleModel.ts
+++ infrastructure/models/articleModel.ts
@@ -1,31 +1,31 @@
 import { ArticleAttributes } from "@core/utils/Article/types";
 import {
-    Table,
-    Column,
-    Model,
-    DataType,
-    CreatedAt,
-    UpdatedAt,
-    AutoIncrement,
-    PrimaryKey,
+  Table,
+  Column,
+  Model,
+  DataType,
+  CreatedAt,
+  UpdatedAt,
+  AutoIncrement,
+  PrimaryKey,
 } from "sequelize-typescript";
 
 // Model for Articles
 @Table({
-    tableName: "articles",
+  tableName: "articles",
 })
 export class Article extends Model<ArticleAttributes> {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
-    id!: number;
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
+  id!: number;
 
-    @Column(DataType.STRING)
-    content!: string;
+  @Column(DataType.TEXT)
+  content!: string;
 
-    @CreatedAt
-    createdAt?: Date;
+  @CreatedAt
+  createdAt?: Date;
 
-    @UpdatedAt
-    updatedAt?: Date;
-}  
+  @UpdatedAt
+  updatedAt?: Date;
+}
